3aba0f3ae97e39893da9b52e09ba175e3772e83f
Michael <MitchProgramming@gmail.com>
2025-05-19 17:39:34 +0200

    Added volume control to waybar through the amixer command and a custom script

---
+++ custom_scripts/waybar_volume_control.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# ~/.config/custom_scripts/waybar_volume_control.sh
+
+VOLUME=$(amixer get Master | grep -oP '\[\d+%\]' | head -1 | tr -d '[%]')
+
+yad --scale \
+    --text="Volume" \
+    --value="$VOLUME" \
+    --min-value=0 \
+    --max-value=100 \
+    --step=1 \
+    --print-partial \
+    --on-top \
+    --skip-taskbar \
+    --undecorated \
+    --no-buttons \
+    --width=200 \
+    --height=50 \
+    | while read vol; do
+        amixer sset 'Master' "$vol%"
+    done
