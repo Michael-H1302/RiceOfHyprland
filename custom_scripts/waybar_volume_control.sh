202fd3831f9c86339d1d63224e1acd2251329d46
Michael-H1302 <MitchProgramming@gmail.com>
2025-12-31 17:20:02 +0100

    Fixed a bug with the volume control

--- custom_scripts/waybar_volume_control.sh
+++ custom_scripts/waybar_volume_control.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-
+# ~/.config/custom_scripts/waybar_volume_control.sh
 VOLUME=$(amixer get Master | grep -oP '\[\d+%\]' | head -1 | tr -d '[%]')
-
 yad --scale \
     --text="Volume" \
     --title="Volume" \
-    --class="volume-popup" \
     --value="$VOLUME" \
     --min-value=0 \
     --max-value=100 \
     --step=1 \
     --print-partial \
+    --on-top \
+    --skip-taskbar \
+    --undecorated \
     --no-buttons \
     --width=200 \
     --height=50 \
-    --window-icon=audio-volume-high
+    | while read vol; do
+        amixer sset 'Master' "$vol%"
+    done
