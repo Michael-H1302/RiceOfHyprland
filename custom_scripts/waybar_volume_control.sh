2c5929f81f418ba73b3de767f934d726dd576d99
Michael-H1302 <MitchProgramming@gmail.com>
2025-12-31 17:00:30 +0100

    Fixed floating behaviour for volume control

--- custom_scripts/waybar_volume_control.sh
+++ custom_scripts/waybar_volume_control.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
-# ~/.config/custom_scripts/waybar_volume_control.sh
 
 VOLUME=$(amixer get Master | grep -oP '\[\d+%\]' | head -1 | tr -d '[%]')
 
 yad --scale \
     --text="Volume" \
+    --title="Volume" \
+    --class="volume-popup" \
     --value="$VOLUME" \
     --min-value=0 \
     --max-value=100 \
     --step=1 \
     --print-partial \
-    --on-top \
-    --skip-taskbar \
-    --undecorated \
     --no-buttons \
     --width=200 \
     --height=50 \
-    | while read vol; do
-        amixer sset 'Master' "$vol%"
-    done
+    --window-icon=audio-volume-high
